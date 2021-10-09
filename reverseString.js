3985c05ea70c45e8392df080940040d3b0aeeaa9
Abdullah-Al-Turky <47826280+coderabdullahturki@users.noreply.github.com>
2021-10-09 16:30:18 +0600

    updated

---
+++ reverseString.js
@@ -0,0 +1,6 @@
+function reverseString(x) {
+    // x is a string
+    // return a string
+    // (ex. x="Hello from Toptal", you should return "latpoT morf olleH")
+    return x.split("").reverse().join("");
+  };
